3b8832dfa86093b92f80ccdf8b5e0bf7ce51ce8a
Felipe Fonseca <fpfonseca54@gmail.com>
2026-04-30 21:30:01 -0300

    Atualizacoes 2026-04-30 21:29

---
+++ listas-exercicios/05-lista/05-lista-resolucao.R
@@ -0,0 +1,6 @@
+# Arquivo: 04-lista-resolucao.R
+# Autor(a): Felipe Fonseca
+# Data: 17/04/2026
+# Objetivo: Resolução da lista de exercícios 4
+
+# Configuracoes globais  ------------------------------------
